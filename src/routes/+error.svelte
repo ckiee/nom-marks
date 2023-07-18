d8b3c1c5af6d7fca8643e964c211e4f2882a600b
ckie <git-525ff67@ckie.dev>
2023-07-18 07:53:31 +0300

    error handler that's accessible

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,5 +1,10 @@
 <script>
 	import { page } from '$app/stores';
+    import { writable } from "svelte/store";
+	import { onMount } from 'svelte';
+
+    let last = writable();
+    onMount(() => last.set(document.referrer));
 </script>
 
 <h1 class="font-semibold w-fit mx-auto text-lg text-red-500 bg-slate-200 p-2 rounded">
@@ -7,7 +12,7 @@
 
     <div class="text-emerald-600 font-light">
         sorry!
-        <a href="#" on:click={() => history.back()}>try going back?</a>
+        <a href={$last}>try going back?</a>
     </div>
 </h1>
 
